2dee43d1f4d599bf46cc4aaf9bd745eadf29a48e
deydrums <dagarcia100@gmail.com>
2021-07-12 14:16:25 -0600

    Multiples exportaciones

---
+++ React/02-intro-javascript/src/bases/08-imp-exp.js
@@ -0,0 +1,24 @@
+//import {heroes} from '../src/data/heroes';
+//import  {heroes} from './data/heroes'
+
+import heroes, { owners } from "../data/heroes";
+
+// const getHeroeById = (id) => {
+//     return heroes.find((heroe)=>{
+//         if(heroe.id === id){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     });
+// }
+
+const getHeroeById = (id) => heroes.find((heroe)=> heroe.id === id);
+console.log(getHeroeById(1));
+
+console.log('______________________');
+
+const getHeroesByOwner = (owner) => heroes.filter((heroe)=> heroe.owner === owner);
+console.log(getHeroesByOwner('DC'));
+
+console.log(owners);
