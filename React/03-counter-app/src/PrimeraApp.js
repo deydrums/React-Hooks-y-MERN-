d3b3f3f02ad6e79f057123bebbf52c0cc9b597a1
deydrums <dagarcia100@gmail.com>
2021-07-13 09:44:37 -0600

    Retornar elementos en el componente

--- React/03-counter-app/src/PrimeraApp.js
+++ React/03-counter-app/src/PrimeraApp.js
@@ -1,8 +1,16 @@
 // SFC
+//import React, { Fragment } from 'react';
 import React from 'react';
 
 const PrimeraApp = () =>{
-    return <h1>Hola mundo</h1>;
+    return (
+        <>
+            <h1>Hola mundo</h1>
+            <p>Mi primera aplicacion</p>
+        </>
+    );
+    
+    
 }
 
 export default PrimeraApp;
