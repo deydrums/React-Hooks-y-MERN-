8e35fe324a86ac1e0ce8087001a62ffd05c15b99
deydrums <dagarcia100@gmail.com>
2021-07-13 11:36:36 -0600

    Componente CounterApp

--- React/03-counter-app/src/index.js
+++ React/03-counter-app/src/index.js
@@ -2,9 +2,10 @@ import React from  'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraApp';
 import './index.css';
+import CounterApp from './CounterApp';
 
 const saludo = <h1>Hola mundo</h1>;
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render(<PrimeraApp saludo = "Hola mundo"></PrimeraApp>, divRoot);
+ReactDOM.render(<CounterApp value = {123}></CounterApp>, divRoot);
 
