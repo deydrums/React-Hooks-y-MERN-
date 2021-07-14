b439fca0042c4820099459bd743ab5dd715f753e
deydrums <dagarcia100@gmail.com>
2021-07-13 19:03:23 -0600

    toEqual

---
+++ React/03-counter-app/src/tests/base/05-funciones.test.js
@@ -0,0 +1,25 @@
+import { getUser, getUsuarioActivo } from "../../base/05-funciones"
+import '@testing-library/jest-dom';
+
+describe('Pruebas en 05-funciones', () => {
+    test('getUser debe de retornar un objeto', () => {
+        const userTest = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+        expect (user).toEqual(userTest);
+    })
+
+    test('getUsuarioActivo debe de retornar un objeto nombre', () => {
+        const nombre = 'David';
+        const userTest = {
+            uid: 'ABC567',
+            username: nombre
+        }
+
+        const user = getUsuarioActivo(nombre);
+        expect (user).toEqual(userTest);
+    })
+})
