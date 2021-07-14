9626bc86f1422a9e338f350c96c6996ae19af42e
deydrums <dagarcia100@gmail.com>
2021-07-14 15:46:28 -0600

    Enzyme - Testing unit

--- React/03-counter-app/src/tests/PrimeraApp.test.js
+++ React/03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,12 +1,23 @@
+
 import { render } from "@testing-library/react";
-import PrimeraApp from "../PrimeraApp";
 import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from "enzyme";
+import PrimeraApp from "../PrimeraApp";
+
 
 describe('Pruebas en <PrimeraApp/>', () => {
-    test('Demostrar el mensaje "Hola, Soy Goku"', () => {
+    // test('Demostrar el mensaje "Hola, Soy Goku"', () => {
+    //     const saludo = 'Hola, soy Goku';
+    //     //const wrapper = render(<PrimeraApp saludo ={saludo} />)
+    //     const {getByText} = render(<PrimeraApp saludo ={saludo} />)
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // })
+
+    test('Debe de mostrar <PrimeraApp /> correctamente', () => {
         const saludo = 'Hola, soy Goku';
-        //const wrapper = render(<PrimeraApp saludo ={saludo} />)
-        const {getByText} = render(<PrimeraApp saludo ={saludo} />)
-        expect(getByText(saludo)).toBeInTheDocument();
+        const wrapper = shallow(<PrimeraApp saludo = {saludo}/>);
+        expect (wrapper).toMatchSnapshot();
     })
+    
 })
