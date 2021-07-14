8fd493d08caaa7f7e1984c44ef342d4e52ad35cb
deydrums <dagarcia100@gmail.com>
2021-07-14 13:55:10 -0600

    Pruebas con tareas asincronas

--- React/03-counter-app/src/base/08-imp-exp.js
+++ React/03-counter-app/src/base/08-imp-exp.js
@@ -1,20 +1,14 @@
 // import { heroes } from './data/heroes';
 // import { heroes } from './data/heroes';
 // import heroes, { owners } from '../data/heroes';
-import heroes from '../data/heroes';
-
-// console.log( owners );
-
+import heroes from '../data/heroes.js';
 
 
 
 export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
 
-// console.log( getHeroeById(2) );
+
 
 // find?, filter
 export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
-
-// console.log( getHeroesByOwner('Marvel') );
-
 
