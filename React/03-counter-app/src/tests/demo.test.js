ee57b4ddc264564f3072b77e202add6ce3241536
deydrums <dagarcia100@gmail.com>
2021-07-13 18:04:43 -0600

    Jest Expect toBe

--- React/03-counter-app/src/tests/demo.test.js
+++ React/03-counter-app/src/tests/demo.test.js
@@ -1,6 +1,13 @@
-test('Debe de ser true ', () => {
-    const isActive = true;
-    if(isActive) {
-        throw new Error('No esta activo')
-    }
-})
+describe('Pruebas en el archivo demo.test.js', () => {
+    
+    test('Deben de ser iguales los string ', () => {
+        //1.Inicializacion
+        const mensaje = 'Hola Mundo';
+        //2.Estimulo
+        const mensaje2 = "Hola Mundo";
+        //3.Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);// ===
+    })
+    
+});
+
