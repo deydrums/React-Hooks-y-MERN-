ae98e8a683217765d8d3ea007806ad469cc676ad
deydrums <dagarcia100@gmail.com>
2021-07-15 11:41:51 -0600

    Fetch Api

--- React/04-gif-expert-app/src/GifExpertApp.js
+++ React/04-gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 const GifExpertApp = () =>{
 
     //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
-    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    const [categories, setCategories] = useState(['One Punch']);
     
     // const handleAdd = () =>{
     //     setCategories([...categories, 'HunterXHunter']);
@@ -21,9 +22,12 @@ const GifExpertApp = () =>{
             {/* <button onClick={handleAdd}>Agregar</button> */}
             <ol>
                 {
-                    categories.map(category => {
-                        return <li key = {category}>{category}</li>
-                    })
+                    categories.map(category => (
+                        <GifGrid
+                            category = {category}
+                            key = {category}
+                        />
+                    ))
                 }
             </ol>
         </>
