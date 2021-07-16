53b7e53ff19a26763d9976c4310e187989aa071e
deydrums <dagarcia100@gmail.com>
2021-07-16 14:29:05 -0600

    Pruebas del componente GifGrid-Mock customHook

---
+++ React/04-gif-expert-app/src/tests/components/GifGrid.test.js
@@ -0,0 +1,36 @@
+import { GifGrid } from "../../components/GifGrid"
+import { shallow } from "enzyme";
+import React from 'react';
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+jest.mock('../../hooks/useFetchGifs');
+
+describe('Pruebas en el componente <GifGrid></GifGrid>', () => {
+    const category = 'Dragon Ball';
+
+    test('Debe de mostrarse el componente <GifGrid></GifGrid>', () => {
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        });
+        const wrapper = shallow(<GifGrid category = {category}/>);
+        expect (wrapper).toMatchSnapshot();
+    });
+    test('Debe de mostrar items cuando se cargan imagenes useFetchGifs', () => {
+        
+        const gifs = [{
+            id: 'ABC',
+            url: 'https://google.com',
+            title: 'ABC Title'
+        }]
+        
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false,
+        });
+        
+        const wrapper = shallow(<GifGrid category={category}/>);
+ 
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+})
