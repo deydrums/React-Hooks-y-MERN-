5f872f383b7b86afe2898b8ef63b360406467d3f
deydrums <dagarcia100@gmail.com>
2021-07-16 14:38:47 -0600

    Evaluar si existen componentes

--- React/04-gif-expert-app/src/tests/components/GifGrid.test.js
+++ React/04-gif-expert-app/src/tests/components/GifGrid.test.js
@@ -1,4 +1,5 @@
 import { GifGrid } from "../../components/GifGrid"
+import '@testing-library/jest-dom';
 import { shallow } from "enzyme";
 import React from 'react';
 import { useFetchGifs } from "../../hooks/useFetchGifs";
@@ -29,8 +30,9 @@ describe('Pruebas en el componente <GifGrid></GifGrid>', () => {
         });
         
         const wrapper = shallow(<GifGrid category={category}/>);
- 
-        expect(wrapper).toMatchSnapshot();
+        expect (wrapper).toMatchSnapshot();
+        expect(wrapper.find('p').exists()).toBe(false);
+        expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
     });
     
 })
