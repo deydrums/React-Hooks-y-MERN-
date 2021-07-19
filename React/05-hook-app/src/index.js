78b5fda8f087544a51bb1104839f4583d6724575
deydrums <dagarcia100@gmail.com>
2021-07-19 10:47:32 -0600

    useRef

--- React/05-hook-app/src/index.js
+++ React/05-hook-app/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 //import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //    "start": "set BROWSER=chrome&& react-scripts start",
 
 ReactDOM.render(
-    <MultipleCustomHooks />,
+    <FocusScreen />,
   document.getElementById('root')
 );
 
