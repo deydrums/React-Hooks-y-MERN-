aa994de79328cc28489981a144f9461f169728f7
deydrums <dagarcia100@gmail.com>
2021-07-19 14:20:27 -0600

    useReducer - TodoList

--- React/05-hook-app/src/index.js
+++ React/05-hook-app/src/index.js
@@ -1,25 +1,25 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Padre } from './components/07-tarea-memo/Padre';
-// //import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// // import { HookApp } from './HookApp';
-// //import { CounterApp } from './components/01-useState/CounterApp';
-// //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-// //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-// //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-// //import { FocusScreen } from './components/04-useRef/FocusScreen';
-// //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-// //import { Layout } from './components/05-useLayoutEffect/Layout';
-// //import { Memorize } from './components/06-memos/Memorize';
-// //import { MemoHook } from './components/06-memos/MemoHook';
-// //import { CallbackHook } from './components/06-memos/CallbackHook';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { TodoApp } from './components/08-useReducer/TodoApp';
+//import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { HookApp } from './HookApp';
+//import { CounterApp } from './components/01-useState/CounterApp';
+//import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { FocusScreen } from './components/04-useRef/FocusScreen';
+//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+//import { Layout } from './components/05-useLayoutEffect/Layout';
+//import { Memorize } from './components/06-memos/Memorize';
+//import { MemoHook } from './components/06-memos/MemoHook';
+//import { CallbackHook } from './components/06-memos/CallbackHook';
+//import { Padre } from './components/07-tarea-memo/Padre';
 
 
-// //    "start": "set BROWSER=chrome&& react-scripts start",
+//    "start": "set BROWSER=chrome&& react-scripts start",
 
-// ReactDOM.render(
-//     <Padre />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+    <TodoApp />,
+  document.getElementById('root')
+);
 
-import './components/08-useReducer/intro-reducer';
