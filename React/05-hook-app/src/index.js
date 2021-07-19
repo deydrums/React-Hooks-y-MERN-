3f53cbac94a73870182c41bbccb4193a8be23d89
deydrums <dagarcia100@gmail.com>
2021-07-19 12:01:56 -0600

    useCallback

--- React/05-hook-app/src/index.js
+++ React/05-hook-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MemoHook } from './components/06-memos/MemoHook';
+import { CallbackHook } from './components/06-memos/CallbackHook';
 //import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
 //import { CounterApp } from './components/01-useState/CounterApp';
@@ -11,12 +11,13 @@ import { MemoHook } from './components/06-memos/MemoHook';
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { Layout } from './components/05-useLayoutEffect/Layout';
 //import { Memorize } from './components/06-memos/Memorize';
+//import { MemoHook } from './components/06-memos/MemoHook';
 
 
 //    "start": "set BROWSER=chrome&& react-scripts start",
 
 ReactDOM.render(
-    <MemoHook />,
+    <CallbackHook />,
   document.getElementById('root')
 );
 
