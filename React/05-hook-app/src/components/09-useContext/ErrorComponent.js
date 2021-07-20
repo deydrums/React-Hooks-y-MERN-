93139f25db185993081549491051e0a84f28f82a
deydrums <dagarcia100@gmail.com>
2021-07-19 18:56:10 -0600

    Link y NavLink

---
+++ React/05-hook-app/src/components/09-useContext/ErrorComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ErrorComponent = () => {
+    return (
+        <div className="alert alert-danger text-center">
+            <h1 >404 Pagina no encontrada</h1>
+        </div>
+    )
+}
