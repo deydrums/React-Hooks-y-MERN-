abbc6eceed082fb5f4276196db97255b40b798be
deydrums <dagarcia100@gmail.com>
2021-07-20 18:35:27 -0600

    Interacciones con el useState

---
+++ React/05-hook-app/src/tests/components/04-useRef/RealExampleRef.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { RealExampleRef } from "../../../components/04-useRef/RealExampleRef";
+import {shallow} from 'enzyme';
+
+describe('Pruebas en <RealExampleRef/>', () => {
+    const wrapper = shallow(<RealExampleRef/>);
+
+    test('Debe mostrarse correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('MultipleCustomHooks').exists()).toBe(false);
+    });
+
+    test('Debe mostrare el componente <MultipleCustomHooks/>', () => {
+        const button = wrapper.find('button');
+        button.simulate('click');
+        console.log(wrapper.find('MultipleCustomHooks'));
+        expect(wrapper.find('MultipleCustomHooks').exists()).toBe(true);
+
+        //expect(wrapper.find('p').text().trim()).toBe(value);
+    });  
+})
