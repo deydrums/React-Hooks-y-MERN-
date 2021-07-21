c52bbf9680b40aa7b0c5470ffa193eba253f1dc9
deydrums <dagarcia100@gmail.com>
2021-07-21 16:37:02 -0600

    Maquetacion Hero

--- React/07-heroes-app/src/components/heroes/HeroScreen.js
+++ React/07-heroes-app/src/components/heroes/HeroScreen.js
@@ -28,28 +28,29 @@ export const HeroScreen = () => {
     } = hero;
 
     return (
-        <div className = "row mt-5">
-            <div className = "col-4">
+        <div className = "container container-heroe">
+            <div className = "container-img">
                 <img
                     src = {`../assets/heroes/${heroeId}.jpg`}
                     alt = {superhero}
-                    className = "img-thumbnail"
                 />
             </div>
-            <div className = "col-8">
-                <h3>{superhero}</h3>
+            <div className = "container-info">
+                <h3 className="mt-2">{superhero}</h3>
+                <h5 className="mt-2">Datos</h5>
                 <ul className = "list-group list-group-flush">
-                    <li className = "list-group-item">{alter_ego}</li>
-                    <li className = "list-group-item">{publisher}</li>
-                    <li className = "list-group-item">{first_appearance}</li>
+                    <li className = "list-group-item"><span className="badge bg-secondary m-1">Nombre original: </span>{alter_ego}</li>
+                    <li className = "list-group-item"><span className="badge bg-secondary m-1">Editorial: </span>{publisher}</li>
+                    <li className = "list-group-item"><span className="badge bg-secondary m-1">Por primera vez en: </span> {first_appearance}</li>
                 </ul>
-                <h5>Characters</h5>
-                <p>{characters}</p>
+                <h5 className="mt-2">Nombres</h5>
+                <p className = "list-group-item"> {characters}</p>
+
                 <button 
-                    className = "btn btn-outline-primary"
+                    className = "btn btn-outline-primary mibutton"
                     onClick={handleReturn}
                 >
-                    Return
+                    Volver
                 </button>
             </div>
 
