b18715a650bcd8e001de24051e33f6f174f7abf9
deydrums <dagarcia100@gmail.com>
2021-07-21 11:47:38 -0600

    Navbar responsive

--- React/07-heroes-app/src/components/ui/Navbar.js
+++ React/07-heroes-app/src/components/ui/Navbar.js
@@ -3,50 +3,55 @@ import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Asociaciones
-            </Link>
-
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
-
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/marvel"
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link 
+                        className="navbar-brand" 
+                        to="/"
                     >
-                        Marvel
-                    </NavLink>
+                        Asociaciones
+                    </Link>
 
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <NavLink 
+                                activeClassName="active"
+                                className="nav-item nav-link" 
+                                exact
+                                to="/marvel"
+                            >
+                                Marvel
+                            </NavLink>
+
+                            <NavLink 
+                                activeClassName="active"
+                                className="nav-item nav-link" 
+                                exact
+                                to="/dc"
+                            >
+                                DC
+                            </NavLink>
+                        </div>
+
+                        <div className="navbar-nav navbar-right">
+                        
+                            <NavLink 
+                            activeClassName="active"
+                            className="nav-item nav-link" 
+                            exact
+                            to="/login"
+                            >
+                                Logout
+                            </NavLink>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </nav>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
-                    >
-                        Logout
-                    </NavLink>
-                </ul>
-            </div>
-        </nav>
     )
 }
