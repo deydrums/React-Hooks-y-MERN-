f358b5f7d62d49d527ed68fbf3e354a9cbc154f3
deydrums <dagarcia100@gmail.com>
2021-07-23 16:51:42 -0600

    Sidebar

--- React/08-journal-app/src/components/journal/Sidebar.js
+++ React/08-journal-app/src/components/journal/Sidebar.js
@@ -4,9 +4,18 @@ export const Sidebar = () => {
     return (
         <aside className = "journal__sidebar">
             <div className="journal__sidebar-navbar">
-                <h3>
-                    <span>David</span>
+                <h3 className = "mt-5">
+                    <i className="fa fa-moon"></i>
+                    <span> David</span>
                 </h3>
+
+                <button className="btn">
+                    Logout
+                </button>
+            </div>
+            <div className="journal__new-entry">
+                <i className="far fa-calendar-plus fa-5x"></i>
+                <p className = "mt-5">Nueva entrada</p>
             </div>
         </aside>
     )
