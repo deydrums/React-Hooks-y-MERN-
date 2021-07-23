1d4c7cf0e708eafc4f017a47454bb306c9454980
deydrums <dagarcia100@gmail.com>
2021-07-23 14:01:46 -0600

    Implementado Rutas

---
+++ React/08-journal-app/src/routers/AppRouter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Redirect
+  } from "react-router-dom";
+import { JournalScreen } from '../components/journal/JournalScreen';
+import { AuthRouter } from './AuthRouter';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/auth" component ={AuthRouter}/>
+                <Route exact path="/" component ={JournalScreen}/>
+                <Redirect to = "/auth/login"/>
+            </Switch>
+        </Router>
+    )
+}
