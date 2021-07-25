604c2d2148c133c0e2f7df7fda649c875cfc3588
deydrums <dagarcia100@gmail.com>
2021-07-25 16:42:41 -0600

    Mantener el usuario

--- React/08-journal-app/src/routers/AppRouter.js
+++ React/08-journal-app/src/routers/AppRouter.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import firebase from 'firebase';
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
 import {
     HashRouter as Router,
     Switch,
     Route,
     Redirect
   } from "react-router-dom";
+import { login } from '../actions/auth';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user?.uid){
+                dispatch(login(user.uid, user.displayName));
+            };
+        });
+        
+    }, []);
+    
     return (
         <Router>
             <Switch>
