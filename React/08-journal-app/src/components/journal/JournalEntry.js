793b8cbc08d88723cb44b745cda8a9137eacd1c2
deydrums <dagarcia100@gmail.com>
2021-07-26 11:34:37 -0600

    Mostrar las notas

--- React/08-journal-app/src/components/journal/JournalEntry.js
+++ React/08-journal-app/src/components/journal/JournalEntry.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({id, date, title, body, url}) => {
+    moment.defineLocale('es', {
+        months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+        monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
+        weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
+        weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+        weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
+      })
+    const noteDate = moment(date);
+    console.log(noteDate);
     return (
         <div className="journal__entry pointer">
-            <div 
-                className="journal__entry-picture"
-                style = {{
-                    backgroundSize: 'cover',
-                    backgroundImage: 'url(https://tipsmake.com/data1/thumbs/how-to-extract-img-files-in-windows-10-thumb-bzxI4IDgg.jpg)'
-                }}
-            >
-            </div>
+            {
+                url &&
+                <div 
+                    className="journal__entry-picture"
+                    style = {{
+                        backgroundSize: 'cover',
+                        backgroundImage: `url(${url})`
+                    }}
+                >
+            
+                </div>
+            }
             <div className="journal__entry-body">
                 <p className="journal__entry-title">
-                    Un nuevo dia
+                    {title}
                 </p>
                 <p className="journal__entry-content">
-                    loremIpsum dolor sit amet, consectetur adipisicing element
+                    {body}
                 </p>
             </div>
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{noteDate.format('dddd')}</span>
+                <h4>{noteDate.format('Do MMM')}</h4>
             </div>
         </div>
     )
