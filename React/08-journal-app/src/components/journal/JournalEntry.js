a5d132901ffbefc836bbbe898527784e04f56fb2
deydrums <dagarcia100@gmail.com>
2021-07-26 11:46:04 -0600

    updateLocale

--- React/08-journal-app/src/components/journal/JournalEntry.js
+++ React/08-journal-app/src/components/journal/JournalEntry.js
@@ -2,15 +2,15 @@ import React from 'react';
 import moment from 'moment';
 
 export const JournalEntry = ({id, date, title, body, url}) => {
-    moment.defineLocale('es', {
+    moment.updateLocale('es', {
         months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
         monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
         weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
         weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
         weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
       })
+      moment.updateLocale('es','en');
     const noteDate = moment(date);
-    console.log(noteDate);
     return (
         <div className="journal__entry pointer">
             {
