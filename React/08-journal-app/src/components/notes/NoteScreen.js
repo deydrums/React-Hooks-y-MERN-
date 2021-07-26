9b013ef41afb676ebca50fbf3d771a3dbc001ee1
deydrums <dagarcia100@gmail.com>
2021-07-26 12:25:25 -0600

    Actualizar nota actual

--- React/08-journal-app/src/components/notes/NoteScreen.js
+++ React/08-journal-app/src/components/notes/NoteScreen.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
 
+    const dispatch = useDispatch();
     const {active: note} = useSelector(state => state.notes);
     const [formValues, handleInputChange, reset] = useForm(note);
     
@@ -16,6 +18,10 @@ export const NoteScreen = () => {
         }
     }, [note, reset])
 
+    useEffect(() => {
+        dispatch(activeNote(formValues.id,{...formValues}));
+    }, [formValues])
+
     const {body,title} =formValues;
     return (
         <div className = "notes__main-content">
@@ -24,6 +30,7 @@ export const NoteScreen = () => {
                     <input
                         type = "text"
                         placeholder = "Título..."
+                        name = 'title'
                         className = "notes__title-input"
                         value ={title}
                         onChange={handleInputChange}
@@ -32,6 +39,7 @@ export const NoteScreen = () => {
                     <textarea
                         placeholder = "Que paso hoy?"
                         className = "notes__textarea"
+                        name = 'body'
                         value ={body}
                         onChange={handleInputChange}
                     >
