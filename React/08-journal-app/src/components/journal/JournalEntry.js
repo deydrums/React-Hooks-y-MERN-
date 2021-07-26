37b3a57ba6cfca4519572f3e7d15417eda650282
deydrums <dagarcia100@gmail.com>
2021-07-26 12:03:48 -0600

    Activar nota para editar

--- React/08-journal-app/src/components/journal/JournalEntry.js
+++ React/08-journal-app/src/components/journal/JournalEntry.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
 export const JournalEntry = ({id, date, title, body, url}) => {
+    const dispatch = useDispatch(); 
     moment.updateLocale('es', {
         months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
         monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
@@ -9,10 +12,15 @@ export const JournalEntry = ({id, date, title, body, url}) => {
         weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
         weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
       })
-      moment.updateLocale('es','en');
     const noteDate = moment(date);
+    const handleEntryClick = () => {
+        dispatch(activeNote(id,{
+            date,title,body,url
+        }));
+    }
+
     return (
-        <div className="journal__entry pointer">
+        <div className="journal__entry pointer" onClick={handleEntryClick}>
             {
                 url &&
                 <div 
--- React/08-journal-app/src/components/notes/NoteScreen.js
+++ React/08-journal-app/src/components/notes/NoteScreen.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+
+    const {active: note} = useSelector(state => state.notes);
+    const [formValues, handleInputChange, reset] = useForm(note);
+    console.log(formValues);
+    const {body,title} =formValues;
     return (
         <div className = "notes__main-content">
             <NotesAppBar />
@@ -10,20 +17,28 @@ export const NoteScreen = () => {
                         type = "text"
                         placeholder = "Título..."
                         className = "notes__title-input"
+                        value ={title}
+                        onChange={handleInputChange}
+                        
                     />
                     <textarea
                         placeholder = "Que paso hoy?"
                         className = "notes__textarea"
+                        value ={body}
+                        onChange={handleInputChange}
                     >
 
                     </textarea>
 
-                    <div className = "notes__image">
-                        <img 
-                            src = "https://deydrums.com/proyectos/react-hereosapp/static/media/marvel-iron.b394df11.jpg"
-                            alt = "image"
-                        />
-                    </div>
+                    {  
+                        (note.url) &&
+                        <div className = "notes__image">
+                            <img 
+                                src = "https://deydrums.com/proyectos/react-hereosapp/static/media/marvel-iron.b394df11.jpg"
+                                alt = "image"
+                            />
+                        </div>
+                    }
 
             </div>
         </div>
