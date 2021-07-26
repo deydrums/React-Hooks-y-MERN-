b7b92534ffc0d27bbd840c47288f6db3b0c9c680
deydrums <dagarcia100@gmail.com>
2021-07-26 16:55:04 -0600

    Animaciones

--- React/08-journal-app/src/components/journal/JournalScreen.js
+++ React/08-journal-app/src/components/journal/JournalScreen.js
@@ -24,7 +24,7 @@ export const JournalScreen = () => {
                 {/* <NothingSelected/> */}
                 {
                     (active)
-                        ? (<NoteScreen />)
+                        ? (<NoteScreen/>)
                         : (<NothingSelected/>)
                 }
                 
