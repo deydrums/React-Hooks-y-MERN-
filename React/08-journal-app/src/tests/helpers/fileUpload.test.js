9a9a89dd929f4559226c1e8a9199b52d1b46c1d0
deydrums <dagarcia100@gmail.com>
2021-07-26 22:11:30 -0600

    Pruebas de carga de archivos

---
+++ React/08-journal-app/src/tests/helpers/fileUpload.test.js
@@ -0,0 +1,23 @@
+import { fileUpload } from "../../helpers/fileUpload";
+
+describe('Pruebas en fileUpload', () => {
+
+    test('Debe de cargar un archivo y retornar el URL', async() => {
+        const resp = await fetch('https://19yw4b240vb03ws8qm25h366-wpengine.netdna-ssl.com/wp-content/uploads/10-Free-To-Use-CORS-Proxies-1024x768.png');
+        const blob = await resp.blob();
+        const file = new File([blob],'foto.jpg');
+        const url = await fileUpload(file);
+        expect(typeof url).toBe('string');
+    });
+
+    test('Debe de retornar un error', async() => {
+        const file = new File([],'foto.jpg');
+        const url = await fileUpload(file);
+        expect( url).toBe(null);
+    });
+    
+    
+});
+
+
+
---
+++ React/08-journal-app/src/tests/types/types.test.js
@@ -0,0 +1,25 @@
+import { types } from "../../types/types";
+
+describe('Pruebas en types', () => {
+    test('Debe de retornar los types', () => {
+        expect(types).toEqual({
+            login: '[Auth] Login',
+            logout: '[Auth] Logout',
+        
+            uiSetError: '[UI] Set Error',
+            uiRemoveError: '[UI] Remove Error',
+        
+            uiStartLoading: '[UI] Start loading',
+            uiFinishLoading: '[UI] Finish loading',
+        
+            notesAddNew: '[Notes] New note',
+            notesActive: '[Notes] Set active note',
+            notesLoad: '[Notes] Load notes',
+            notesUpdated: '[Notes] Update note',
+            notesFileUrl: '[Notes] Update image url',
+            notesDelete: '[Notes] Delete note',
+            notesLogoutCleaning: '[Notes] Logout Cleaning'
+        });
+    });
+    
+});
