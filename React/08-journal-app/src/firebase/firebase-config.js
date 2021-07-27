62fca9c24d2a045d0c8f74ffbe22144618bb3c25
deydrums <dagarcia100@gmail.com>
2021-07-27 15:39:42 -0600

    Variables de entorno

--- React/08-journal-app/src/firebase/firebase-config.js
+++ React/08-journal-app/src/firebase/firebase-config.js
@@ -3,31 +3,32 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-apiKey: "AIzaSyDj8xrvyOfXjn9qFJYre1Sz4DDEnvwzcQU",
-authDomain: "react-apps-bafe2.firebaseapp.com",
-projectId: "react-apps-bafe2",
-storageBucket: "react-apps-bafe2.appspot.com",
-messagingSenderId: "650936320724",
-appId: "1:650936320724:web:e1b821cca70cead587f141"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
 };
 
-const firebaseConfigTesting = {
-    apiKey: "AIzaSyC15g8qKdulcmjbnoxROJ843TSk2HZimbg",
-    authDomain: "tests-caa2d.firebaseapp.com",
-    projectId: "tests-caa2d",
-    storageBucket: "tests-caa2d.appspot.com",
-    messagingSenderId: "743724009913",
-    appId: "1:743724009913:web:1b3c96d741b92a17a94e68"
-  };
+// const firebaseConfigTesting = {
+//     apiKey: "AIzaSyC15g8qKdulcmjbnoxROJ843TSk2HZimbg",
+//     authDomain: "tests-caa2d.firebaseapp.com",
+//     projectId: "tests-caa2d",
+//     storageBucket: "tests-caa2d.appspot.com",
+//     messagingSenderId: "743724009913",
+//     appId: "1:743724009913:web:1b3c96d741b92a17a94e68"
+//   };
 
-if(process.env.NODE_ENV === 'test') {
-    //testing
-    firebase.initializeApp(firebaseConfigTesting);
-}else{
-    //dev/prod
-    firebase.initializeApp(firebaseConfig);
-}
+// if(process.env.NODE_ENV === 'test') {
+//     //testing
+//     firebase.initializeApp(firebaseConfigTesting);
+// }else{
+//     //dev/prod
+//     firebase.initializeApp(firebaseConfig);
+// }
 
+firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
