fa48efded2811ae7962a3fc22cbe4964cfb9ec5d
deydrums <dagarcia100@gmail.com>
2021-07-26 22:48:10 -0600

    Borrar imagen

--- React/08-journal-app/src/tests/helpers/fileUpload.test.js
+++ React/08-journal-app/src/tests/helpers/fileUpload.test.js
@@ -1,13 +1,25 @@
+import cloudinary from 'cloudinary';
 import { fileUpload } from "../../helpers/fileUpload";
+jest.setTimeout(20000);
+cloudinary.config({ 
+    cloud_name: 'deydrums', 
+    api_key: '938397662797245', 
+    api_secret: 'brYMj1WJisngNKoL3d5Vd528_Wk'
+  });
 
 describe('Pruebas en fileUpload', () => {
 
     test('Debe de cargar un archivo y retornar el URL', async() => {
         const resp = await fetch('https://19yw4b240vb03ws8qm25h366-wpengine.netdna-ssl.com/wp-content/uploads/10-Free-To-Use-CORS-Proxies-1024x768.png');
         const blob = await resp.blob();
-        const file = new File([blob],'foto.jpg');
+        const file = new File([blob],'foto.png');
         const url = await fileUpload(file);
         expect(typeof url).toBe('string');
+        //Borrar imagen por ID
+        const segments = url.split('/');
+        const imageId = segments[segments.length-1].replace('.png','');
+        const { deleted } = await cloudinary.v2.api.delete_resources(imageId);
+        expect(deleted).toEqual({ [imageId]: "deleted" });
     });
 
     test('Debe de retornar un error', async() => {
