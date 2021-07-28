2dfc75a5cbdb0aaace6fddaabb941d83f0e9ad3c
deydrums <dagarcia100@gmail.com>
2021-07-28 11:35:57 -0600

    React Big Calendar

--- React/09-calendar-app/src/components/calendar/CalendarScreen.js
+++ React/09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import { Navbar } from '../ui/Navbar'
+// the imports
+import { Calendar, momentLocalizer  } from 'react-big-calendar' 
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment'
+
+moment.updateLocale('es', {
+    months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+    monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
+    weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
+    weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+    weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
+  })
+
+const localizer = momentLocalizer(moment)
+const events = [{
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add(2, 'hour').toDate(),
+    bgcolor: '#fafafa'
+}]
+
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className = 'calendar-screen'>
             <Navbar/>
+            <div className = 'calendar'>
+                <Calendar 
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                />
+            </div>
+
         </div>
     )
 }
--- React/09-calendar-app/src/index.js
+++ React/09-calendar-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppRouter } from './routers/AppRouter';
-
+import './styles.css';
 
 ReactDOM.render(
     <AppRouter />,
