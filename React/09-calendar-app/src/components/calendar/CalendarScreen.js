be91605e27d7ce4bfac94cb8d887c94cb462d4ac
deydrums <dagarcia100@gmail.com>
2021-07-28 11:45:17 -0600

    Configuraciones adicionales del calendario

--- React/09-calendar-app/src/components/calendar/CalendarScreen.js
+++ React/09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -4,25 +4,40 @@ import { Navbar } from '../ui/Navbar'
 import { Calendar, momentLocalizer  } from 'react-big-calendar' 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment'
-
+import { messages } from '../../helpers/calendar-messages-es';
+import 'moment/locale/es';
 moment.updateLocale('es', {
     months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
     monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
     weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
-    weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+    weekdaysShort: 'Dom_Lun_Mar_Mier_Jue_Vier_Sab'.split('_'),
     weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
   })
+moment.locale('es');
 
 const localizer = momentLocalizer(moment)
 const events = [{
     title: 'Cumpleaños del jefe',
     start: moment().toDate(),
     end: moment().add(2, 'hour').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Comprar pastel'
 }]
 
 
 export const CalendarScreen = () => {
+    const eventStyleGetter = (event, start, end, isSelected) =>{
+        const style ={
+            backgroundColor: '#367CF7',
+            borderRadius:'0px',
+            opacity:0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return{
+            style
+        }
+    }
     return (
         <div className = 'calendar-screen'>
             <Navbar/>
@@ -32,6 +47,8 @@ export const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
+                messages = {messages}
+                eventPropGetter= {eventStyleGetter}
                 />
             </div>
 
