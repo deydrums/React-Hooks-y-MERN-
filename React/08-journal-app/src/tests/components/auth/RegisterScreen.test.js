a71a88c87989d9e6b3695fd458a377203c69aaa8
deydrums <dagarcia100@gmail.com>
2021-07-27 22:10:54 -0600

    Probando que el mensaje de error exista

--- React/08-journal-app/src/tests/components/auth/RegisterScreen.test.js
+++ React/08-journal-app/src/tests/components/auth/RegisterScreen.test.js
@@ -60,5 +60,30 @@ describe('Pruebas en RegisterScreen', () => {
             payload: 'Nombre es requerido'
         }]);
    });
-    
+
+   test('Debe de mostrar la caja de alerta con el error', () => {
+        const initState = {
+            auth:{
+                uid: {},
+            },
+            ui: {
+                loading: false,
+                msgError: 'Email no es correcto'
+            }
+        };
+        
+        const store = mockStore(initState);
+        const wrapper = mount(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <RegisterScreen/>
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect(wrapper.find('.auth__alert-error').exists()).toBe(true);
+        expect(wrapper.find('.auth__alert-error').text().trim()).toBe(initState.ui.msgError);
+
+   });
+
 });
