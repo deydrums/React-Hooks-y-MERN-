82dfe68f85eb77e66d67a674f6a77467c37b252e
deydrums <dagarcia100@gmail.com>
2021-07-29 09:34:45 -0600

    Configurando Express

--- React/10-calendar-backend/index.js
+++ React/10-calendar-backend/index.js
@@ -1,1 +1,16 @@
-console.log('Hola mundo');
+const express = require('express');
+
+//Ejecutar express
+var app = express();
+
+//Rutas
+app.get('/', (req,res)=>{
+    res.json({
+        ok: true,
+    })
+});
+
+//Escuchar peticiones
+app.listen(80,()=>{
+    console.log(`Servicor corriendo en puerto ${4000}`)
+});
