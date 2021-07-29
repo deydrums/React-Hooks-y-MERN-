0c98415740e39a9d7f3c75ccb998b7b21fd64874
deydrums <dagarcia100@gmail.com>
2021-07-29 16:00:19 -0600

    Inicio CRUD

---
+++ React/10-calendar-backend/routes/events.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Rutas de Eventos / events
+ * host + /api/events
+ *  
+ * */ 
+
+ const {Router} = require('express');
+ const router = Router();
+ const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
+ const { validateJWT } = require('../middlewares/validate-jwt');
+ 
+ router.get(
+     '/',
+     validateJWT,
+     getEvents
+     );
+ 
+ router.post(
+    '/',
+    validateJWT,
+    createEvent
+    );
+
+
+ router.put(
+    '/:id',
+    validateJWT,
+    updateEvent
+    );
+ 
+
+router.delete(
+    '/:id',
+    validateJWT,
+    deleteEvent
+    );
+ 
+ module.exports = router;
