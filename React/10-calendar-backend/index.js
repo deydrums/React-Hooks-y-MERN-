ced3b77ad5b3ed7733780f57b196aeaa742c6d6a
deydrums <dagarcia100@gmail.com>
2021-07-29 09:42:46 -0600

    Variables de entorno y carpeta publica

--- React/10-calendar-backend/index.js
+++ React/10-calendar-backend/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
-
+require('dotenv').config();
 //Ejecutar express
 var app = express();
 
+//Directorio publico
+app.use(express.static('public'));
+
 //Rutas
-app.get('/', (req,res)=>{
-    res.json({
-        ok: true,
-    })
-});
+// app.get('/', (req,res)=>{
+//     res.json({
+//         ok: true,
+//     })
+// });
 
 //Escuchar peticiones
-app.listen(80,()=>{
-    console.log(`Servicor corriendo en puerto ${4000}`)
+app.listen(process.env.PORT,()=>{
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 });
