64367b24d7fdadda80a9df48fb3f54476ea1bd41
deydrums <dagarcia100@gmail.com>
2021-07-29 11:01:27 -0600

    Express Validator

--- React/10-calendar-backend/controllers/auth.js
+++ React/10-calendar-backend/controllers/auth.js
@@ -1,10 +1,20 @@
 const {response} = require('express');
+const { validationResult } = require('express-validator');
 
 
 const createUser = (req,res = response)=>{
     const {name, email, password} = req.body;
-    
-    res.json({
+
+    //Manejo de errores
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    };
+
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name,
@@ -15,7 +25,17 @@ const createUser = (req,res = response)=>{
 
 const loginUser = (req,res = response)=>{
     const {email, password} = req.body
-    res.json({
+
+    //Manejo de errores
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    };
+
+    res.status(200).json({
         ok: true,
         msg: 'login',
         email, 
--- React/10-calendar-backend/routes/auth.js
+++ React/10-calendar-backend/routes/auth.js
@@ -6,11 +6,28 @@
  * */ 
 
 const {Router} = require('express');
+const { check } = require('express-validator');
 const router = Router();
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.post('/new', createUser);
-router.post('/', loginUser);
+router.post(
+    '/new',
+    [
+        check('name', 'El nombre no es valido').not().isEmpty(),
+        check('email', 'El email no es valido').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6})
+    ],
+    createUser
+    );
+
+router.post(
+    '/',
+    [
+        check('email', 'El email no es valido').isEmail(),
+        check('password', 'El password debe de poseer minimo 6 caracteres').isLength({min: 6})
+    ],
+     loginUser);
+
 router.get('/renew', renewToken);
 
 module.exports = router;
