4b6656a2396d7981d905dea02bc5a29c5366ece5
deydrums <dagarcia100@gmail.com>
2021-07-29 20:27:34 -0600

    Creando variables de entorno

--- React/09-calendar-app/src/index.js
+++ React/09-calendar-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
 import './styles.css';
 
+console.log(process.env.REACT_APP_API_URL);
 ReactDOM.render(
     <CalendarApp />,
   document.getElementById('root')
