5e35c15ba7642d9ef8f57791ad016305cb1e24dc
deydrums <dagarcia100@gmail.com>
2021-08-09 14:59:55 -0600

    Pruebas en las acciones de Auth

---
+++ React/09-calendar-app/src/tests/actions/auth.test.js
@@ -0,0 +1,40 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import '@testing-library/jest-dom';
+import { startLogin } from '../../actions/auth';
+import { types } from '../../types/types';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+const initState = {};
+let store = mockStore(initState);
+Storage.prototype.setItem = jest.fn();
+
+describe('Pruebas en las acciones del Auth', () => {
+    
+    beforeEach(() => {
+        store = mockStore(initState);
+        jest.clearAllMocks();
+    });
+
+    test('StartLogin correcto', async() => {
+        await store.dispatch(startLogin('dagarcia100@gmail.com', '123456'));
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: types.authLogin,
+            payload: {
+                uid: expect.any(String),
+                name: expect.any(String)
+            }
+        })
+
+        expect(localStorage.setItem).toHaveBeenCalledWith('token',expect.any(String));
+        expect(localStorage.setItem).toHaveBeenCalledWith('token-init-date',expect.any(Number));
+
+        // const token = localStorage.setItem.mock.calls[0][1];
+        // console.log(token);
+        
+    });
+    
+
+});
