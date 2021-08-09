d49e50e56047b8f26f0a1f77bde0de09f9f4cffa
deydrums <dagarcia100@gmail.com>
2021-08-09 16:48:58 -0600

    AppRouter

--- React/09-calendar-app/src/tests/routers/AppRouter.test.js
+++ React/09-calendar-app/src/tests/routers/AppRouter.test.js
@@ -13,18 +13,19 @@ import { AppRouter } from '../../routers/AppRouter';
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-const initState = {
-    auth:{
-        checking: true,
-    }
-};
-let store = mockStore(initState);
-store.dispatch = jest.fn();
-
 describe('Pruebas en <AppRouter />', () => {
 
     test('Debe de mostrar el loader....', async() => {
-        
+
+        const initState = {
+            auth:{
+                checking: true,
+            }
+        };
+
+        const store = mockStore(initState);
+
+
         const wrapper = mount( 
             <Provider store={ store }>
                     <AppRouter /> 
