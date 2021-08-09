132ddd99ed3acc20c47da61eedd659e1ac035e05
deydrums <dagarcia100@gmail.com>
2021-08-09 16:56:47 -0600

    Ruta publica o privada

--- React/09-calendar-app/src/tests/routers/AppRouter.test.js
+++ React/09-calendar-app/src/tests/routers/AppRouter.test.js
@@ -37,5 +37,64 @@ describe('Pruebas en <AppRouter />', () => {
         expect(wrapper).toMatchSnapshot();
 
     });
+
+
+
+    test('Debe de mostrar la ruta publica....', async() => {
+
+        const initState = {
+            auth:{
+                checking: false,
+                uid: null,
+            }
+        };
+
+        const store = mockStore(initState);
+
+
+        const wrapper = mount( 
+            <Provider store={ store }>
+                    <AppRouter /> 
+            </Provider>
+
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.login-container').exists()).toBe(true);
+
+    });
+
+
+
+    test('Debe de mostrar la ruta privada....', async() => {
+
+        const initState = {
+            auth:{
+                checking: false,
+                uid: '123456',
+                name: 'David',
+            },
+            calendar: {
+                events: []
+            },
+            ui: {
+                modalOpen: false,
+            }
+        };
+
+        const store = mockStore(initState);
+
+
+        const wrapper = mount( 
+            <Provider store={ store }>
+                    <AppRouter /> 
+            </Provider>
+
+        );
+        
+        //expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('CalendarScreen').exists()).toBe(true);
+
+    });
     
 });
